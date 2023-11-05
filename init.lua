470d9baf48c398c8db1b4ff5e95c6c11d13ed3ce
Ahmed Riza <dr.riza@gmail.com>
2023-11-05 20:58:47 +0000

    neovim configuration

--- init.lua
+++ init.lua
@@ -5,7 +5,6 @@ vim.api.nvim_set_hl(0, 'TelescopeNormal', {bg='#3B4252'})
 vim.api.nvim_set_hl(0, 'TelescopeBorder', {bg='#3B4252'})
 
 require("core.settings")
-require("core.keymaps")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -22,6 +21,8 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("core.plugins")
 require("core.plugin_config")
+
+require("core.keymaps")
 
 local rt = require("rust-tools")
 rt.setup({
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,3 +1,10 @@
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.keymap.set("n", "<F8>", ":NvimTreeFindFileToggle<CR>")
 vim.keymap.set("n",'<F12>','<cmd>lua vim.lsp.buf.format()<CR>')
+
+-- https://github.com/nvim-telescope/telescope.nvim#getting-started
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
