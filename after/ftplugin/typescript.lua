dff3f5d463748ba32e676ea908a3f0d78ef07e5f
Ahmed Riza <dr.riza@gmail.com>
2026-03-21 14:09:10 +0000

    nvim configuration

--- after/ftplugin/typescript.lua
+++ after/ftplugin/typescript.lua
@@ -1,1 +1,4 @@
 vim.keymap.set("n",'<F12>','<cmd>Neoformat<CR>')
+-- Go to source definition, a new feature introduced in typescript 4.7
+-- and available via the command `TSToolsGoToSourceDefinition` in `typescript-tools` nvim plugin.
+vim.keymap.set("n",'gd','<cmd>TSToolsGoToSourceDefinition<CR>')
